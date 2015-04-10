eb38f925bab45f8538c709c5b5ae67c9ae6ae94d
Julio Raffaine <julio.loxas@gmail.com>
2015-04-10 18:08:25 +0000

    Find the best pair of parameters C and sigma for training SVM on dataset 3

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -9,7 +9,7 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 
 % You need to return the following variables correctly.
 C = 1;
-sigma = 0.3;
+sigma = 0.1;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -22,8 +22,25 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+%lowest = 100000;
 
+%for Ct = [0.01 0.03 0.1 0.3 1 3 10 30]
 
+%for s = [0.01 0.03 0.1 0.3 1 3 10 30]
+
+%model= svmTrain(X, y, Ct, @(x1, x2) gaussianKernel(x1, x2, s));
+%predictions = svmPredict(model, Xval);
+
+%va_cs = mean(double(predictions ~= yval))
+%if( va_cs <= lowest )
+%	lowest = va_cs
+%	C = Ct
+%	sigma = s
+%endif
+
+%end
+
+%end
 
 
 
