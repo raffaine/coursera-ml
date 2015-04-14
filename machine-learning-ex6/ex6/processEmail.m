5ff36026f113d692d03b96f1980a92c45ddd6ad1
Julio Raffaine <julio.loxas@gmail.com>
2015-04-14 08:21:41 +0000

    Process email coding it using indexes on vocabulary List

--- machine-learning-ex6/ex6/processEmail.m
+++ machine-learning-ex6/ex6/processEmail.m
@@ -97,8 +97,10 @@ while ~isempty(email_contents)
     %       str2). It will return 1 only if the two strings are equivalent.
     %
 
-
-
+idx = lookup(vocabList, str, 'm');
+if idx > 0
+	word_indices = [word_indices; idx];
+end
 
 
 
